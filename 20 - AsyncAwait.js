1574458d42e915981ee2d641dc6f285466bdb092
Leonardo Biazus <lbiazus88@gmail.com>
2020-12-12 10:44:07 -0300

    Exemplos acrescentados em aula

--- 20 - AsyncAwait.js
+++ 20 - AsyncAwait.js
@@ -8,9 +8,41 @@ function dividirValores(a,b) {
     })
 }
 
-async function calcular(a,b) {
-    const resultado = await dividirValores(a,b);
-    console.log("Resultado: ", resultado)
+function mostrarPrimeiro() {
+    return "Primeiro";
 }
 
-calcular(100,0);
+function mostrarSegundo() {
+    return new Promise((resolve,reject) => {
+        resolve('Segundo')
+    })
+}
+
+function mostrarTerceiro() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => resolve('Terceiro'),10000)
+    })
+}
+
+console.log(mostrarPrimeiro());
+mostrarSegundo().then(r => console.log(r));
+mostrarTerceiro().then(r => console.log(r));
+
+function dividirValores(a,b) {
+        if (b > 0) {
+            return setTimeout(a/b, 5000);
+        } else {
+            throw new Error(`O valor de b é ${b} e não pode ser usado para divisão`)
+        }
+}
+
+async function calcular(a,b) {
+    try {
+        const resultado = await dividirValores(a,b).then(r => );
+        console.log("Resultado: ", resultado);
+    } catch (e) {
+        console.log("Erro: ", e)
+    }
+}
+
+calcular(100,10);
